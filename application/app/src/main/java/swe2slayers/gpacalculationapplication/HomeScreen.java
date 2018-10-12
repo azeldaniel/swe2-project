bf9145f91ba40f1de07410a05baea876b62dcf9d
Opsidill <chrisjoseph15496@gmail.com>
2018-10-12 17:31:54 -0400

    Added home screen layout and class file. activity_main loads activity_home_screen on launch.

--- application/app/src/main/java/swe2slayers/gpacalculationapplication/HomeScreen.java
+++ application/app/src/main/java/swe2slayers/gpacalculationapplication/HomeScreen.java
@@ -3,13 +3,12 @@ package swe2slayers.gpacalculationapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_screen);
 
     }
-
 }
