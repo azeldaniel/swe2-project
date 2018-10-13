ca235fc141f7e3f6b0675d4fd64d1afd7a59bb34
Opsidill <chrisjoseph15496@gmail.com>
2018-10-13 14:01:38 -0400

    Small change to burndown chart

---
+++ application/app/src/main/java/swe2slayers/gpacalculationapplication/CourseDetail.java
@@ -0,0 +1,13 @@
+package swe2slayers.gpacalculationapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_detail);
+    }
+}
