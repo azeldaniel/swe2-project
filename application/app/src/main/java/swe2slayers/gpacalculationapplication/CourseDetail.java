74f06e57843fe2e22b77c4c8204fc1417ca78c99
Opsidill <chrisjoseph15496@gmail.com>
2018-10-13 14:18:21 -0400

    Added simple version of course detail screen along with its activity class.

--- application/app/src/main/java/swe2slayers/gpacalculationapplication/CourseDetail.java
+++ application/app/src/main/java/swe2slayers/gpacalculationapplication/CourseDetail.java
@@ -9,5 +9,6 @@ public class CourseDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
+
     }
 }
