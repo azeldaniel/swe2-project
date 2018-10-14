0ff89ca9209bbd9135e00fbaf3eb065972767b9a
Michael Bristol <M3618861@hotmail.com>
2018-10-14 00:51:15 -0400

    course detail
    the spinners are the entry of percentage weights

---
+++ application/app/src/main/java/swe2slayers/gpacalculationapplication/Main2Activity.java
@@ -0,0 +1,16 @@
+package swe2slayers.gpacalculationapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semester);
+    }
+
+
+
+}
