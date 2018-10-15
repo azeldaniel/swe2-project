da5ee017d05ad01c5051d3056dab8b8c3f24cc7b
Michael Bristol <M3618861@hotmail.com>
2018-10-15 11:52:24 -0400

    Semester Activity
    added the semster Activity
    apart from this, a suggestion is to let is implement the (already created) Semester Class

---
+++ application/app/src/main/java/swe2slayers/gpacalculationapplication/Semester_Activity.java
@@ -0,0 +1,14 @@
+package swe2slayers.gpacalculationapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Semester_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semester_);
+    }
+
+}
