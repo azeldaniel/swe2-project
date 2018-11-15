59db9a3e36d13ad399a8eb6a1229a9d86adfcee7
Maya Bannis <mayaibannis@gmail.com>
2018-11-15 18:32:50 -0400

    CreateUser Testing - Tutorial
    Seeing if the getters and setters in the User model work as they should.  (More just practice and first tutorial)

---
+++ application/app/src/test/java/swe2slayers/gpacalculationapplication/models/createUserTest.java
@@ -0,0 +1,52 @@
+package swe2slayers.gpacalculationapplication.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+public class createUserTest {
+
+    @Test
+    public void getUserId() {
+        User user= new User();
+        user.setUserId("ON37QKmAbBW0Yu0hXK6jifmScU83");
+        assertTrue(user.getUserId()=="ON37QKmAbBW0Yu0hXK6jifmScU83");
+    }
+
+    @Test
+    public void getEmail() {
+        User user= new User();
+        user.setEmail("test@tests.com");
+        assertTrue(user.getEmail()=="test@tests.com");
+    }
+
+    @Test
+    public void getFirstName() {
+        
+    }
+
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void getStudentId() {
+    }
+
+
+    @Test
+    public void getDegree() {
+    }
+
+
+    @Test
+    public void getTargetGPA() {
+    }
+
+    @Test
+    public void getGradingSchemaId() {
+    }
+
+}
