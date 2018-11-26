4c576f98b5cc2cd1fdc871fc90249586f082d635
Maya Bannis <mayaibannis@gmail.com>
2018-11-26 18:00:34 -0400

    Match format set

--- application/app/src/main/java/swe2slayers/gpacalculationapplication/models/Course.java
+++ application/app/src/main/java/swe2slayers/gpacalculationapplication/models/Course.java
@@ -158,6 +158,6 @@ public class Course implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
-        return this.courseId.equals(((Course)obj).getCourseId());
+        return this.getCourseId().equals(((Course)obj).getCourseId());
     }
 }
